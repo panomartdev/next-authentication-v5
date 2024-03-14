51b237169552104db72a9747ab2e52086a084cd3
PanomArt <artrock548@gmail.com>
2024-03-14 23:07:55 +0700

    first commit

--- middleware.ts
+++ middleware.ts
@@ -1,11 +1,16 @@
-import authConfig from "./auth.config"
-import NextAuth from "next-auth"
-import {DEFAULT_LOGIN_REDIRECT, publicRoutes, authRoutes, apiAuthPrefix} from "@/routes";
+import NextAuth from "next-auth";
+
+import authConfig from "@/auth.config";
+import {
+  DEFAULT_LOGIN_REDIRECT,
+  apiAuthPrefix,
+  authRoutes,
+  publicRoutes,
+} from "@/routes";
 
 const { auth } = NextAuth(authConfig);
 
-
-export default auth((req) => {
+export default auth((req ) => {
   const { nextUrl } = req;
   const isLoggedIn = !!req.auth;
 
@@ -13,33 +18,35 @@ export default auth((req) => {
   const isPublicRoute = publicRoutes.includes(nextUrl.pathname);
   const isAuthRoute = authRoutes.includes(nextUrl.pathname);
 
-  if(isApiAuthRoute) {
-    return null;
+  if (isApiAuthRoute) {
+    return undefined;
   }
 
-  if(isAuthRoute){
-    if(isLoggedIn) {
+  if (isAuthRoute) {
+    if (isLoggedIn) {
       return Response.redirect(new URL(DEFAULT_LOGIN_REDIRECT, nextUrl))
     }
-    return null
+    return undefined;
   }
-  if (!isLoggedIn && !isPublicRoute) {
-    let callbackUrl = nextUrl.pathname
 
-    if(nextUrl.search) {
+  if (!isLoggedIn && !isPublicRoute) {
+    let callbackUrl = nextUrl.pathname;
+    if (nextUrl.search) {
       callbackUrl += nextUrl.search;
     }
 
     const encodedCallbackUrl = encodeURIComponent(callbackUrl);
-    
-    return Response.redirect(new URL(`/auth/login?callbackUrl=${encodedCallbackUrl}`, nextUrl))
+
+    return Response.redirect(new URL(
+      `/auth/login?callbackUrl=${encodedCallbackUrl}`,
+      nextUrl
+    ));
   }
 
-  return null
-
+  return undefined;
 })
 
 // Optionally, don't invoke Middleware on some paths
 export const config = {
-  matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
+  matcher: ['/((?!.+\\.[\\w]+$|_next).*)', '/', '/(api|trpc)(.*)'],
 }
